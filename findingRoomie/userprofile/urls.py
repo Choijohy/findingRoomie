ff52b2826edbf81a27a80a8e8d997c8ae71a5c9b
Choijohy <77671582+Choijohy@users.noreply.github.com>
2021-06-28 12:40:40 +0900

    Update urls.py

--- findingRoomie/userprofile/urls.py
+++ findingRoomie/userprofile/urls.py
@@ -3,6 +3,8 @@ from .views import *
 
 urlpatterns = [
     path('',home,name="home"),
+    path('search', search, name='search'),
+    path('detail/<str:id>',detail,name="detail"),
     path('MyPage/',MyProfile,name="MyPage"),
     path('<int:pk>/edit/',edit,name="edit"),
 ]
