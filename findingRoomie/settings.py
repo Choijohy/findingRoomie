5c332ecd3a1ba1ebe9027b484104ad26508f71a5
Choijohy <77671582+Choijohy@users.noreply.github.com>
2022-02-16 12:33:37 +0900

    Update settings.py

--- findingRoomie/settings.py
+++ findingRoomie/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-k2j7l9t7av-ip7vmz0=64h&4q@3*hwej(4f5#qc!au%cnam+8z'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
